77e0fbb6c76644156daa8659eaadef7733a34caa
mohankgandhi06 <mohankgandhi06@gmail.com>
2018-11-18 20:29:05 +0530

    Changes to the initial commit

--- src/n/chapter/sortingAndSearching/DSortedSearchNoSize.java
+++ src/n/chapter/sortingAndSearching/DSortedSearchNoSize.java
@@ -8,20 +8,27 @@ public class DSortedSearchNoSize {
     Given a Listy which contains sorted, positive integers, find the index at which an element x occurs. If x occurs
     multiple times, you may return any index. */
 
+    /* Algorithm Followed:
+     * 1) Increment each time by 5 steps
+     * 2) Check if either the number is less than the element to find and not -1 and not equal to the element
+     * 3) If the value (elementAt) is either greater than the elementToFind or -1 then take a step back i.e. 5 steps back and
+     * iterate each one by one and linearly compare till the value is found */
+
     public static void main(String[] args) {
         Listy list = new Listy(new int[]{2, 3, 4, 7, 8, 10, 15, 15, 18, 19, 24, 27, 28, 30});
-        int elementToFind = 15;
+        int elementToFind = 16;
         System.out.println("Element is found at index: (if not found then -1 is returned)");
         System.out.println(performSearch(list, elementToFind));
     }
 
     private static int performSearch(Listy list, int elementToFind) {
         int i = 0;
+        int stepSize = 5;
         while (list.elementAt(i) != -1 && list.elementAt(i) != elementToFind && list.elementAt(i) < elementToFind) {
-            i = i + 5;
+            i = i + stepSize;
         }
         if (list.elementAt(i) == -1 || list.elementAt(i) > elementToFind) {//Take a step (4 values back) and iterate each till -1 is reached
-            int j = (i - 5) + 1;
+            int j = (i - stepSize) + 1;
             while (list.elementAt(j) != -1 && list.elementAt(j) != elementToFind) {
                 j++;
             }
